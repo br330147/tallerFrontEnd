25fb5b81e3ec1aa8b3fc3f8d937d4565ee569f1a
alansitomar29 <alansitomar29@gmail.com>
2025-02-21 22:55:29 -0300

    arreglando fecha y paginas2

--- obligatorio/src/components/AgregarActividad.jsx
+++ obligatorio/src/components/AgregarActividad.jsx
@@ -120,7 +120,7 @@ const FormularioRegistro = () => {
         <Form.Label>Fecha</Form.Label>
         <Form.Control 
           type="date"
-          value={obtenerFechaGMT3()}
+          value={fecha}
           onChange={(e) => setFecha(e.target.value)}
           max={obtenerFechaGMT3()}
           />
